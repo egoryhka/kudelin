35f5500db7bcfc89538e40cbbaa463af575dfa5c
egoryhka <egor.demyanenko2000@mail.ru>
2021-10-07 16:34:28 +0300

    камит

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using kudelinSUKA;
+using OfficeOpenXml;
+using System.Linq;
+
+namespace kudelinSUKA_CumСОЛЬ
+{
+    class Program
+    {
+
+
+
+        static void Main(string[] args)
+        {
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+
+            var file = new FileInfo(@"C:\Users\Egor\Desktop\JijaPapka\Jija.xlsx");
+            using (var package = new ExcelPackage(file))
+            {
+                if (package.Workbook.Worksheets.FirstOrDefault(x => x.Name == "My Sheet") == null)
+                {
+                    var sheet = package.Workbook.Worksheets.Add("My Sheet");
+                    sheet.Cells["A1"].Value = "Hello World!";
+                }
+
+
+                // Save to file
+                package.Save();
+            }
+
+
+
+
+
+        }
+    }
+}
